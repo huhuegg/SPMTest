24bb2409a538edac5487e5468ccb88af7d161653
huhuegg <huhuegg@gmail.com>
2016-08-31 19:09:13 +0800

    no message

--- Package.swift
+++ Package.swift
@@ -3,3 +3,6 @@ import PackageDescription
 let package = Package(
     name: "SPMTest"
 )
+
+let libSPMTest = Product(name: "SPMTest", type: .Library(.Dynamic), modules: "SPMTest")
+products.append(libSPMTest)
